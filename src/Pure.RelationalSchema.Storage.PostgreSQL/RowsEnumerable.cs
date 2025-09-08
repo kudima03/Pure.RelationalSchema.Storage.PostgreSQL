c98cb51f8c35e6a41e34cad3acc9477226791620
Dmitry Kurochkin <kudima03@gmail.com>
2025-09-08 17:45:39 +0300

    Fixed wrong query text.

--- src/Pure.RelationalSchema.Storage.PostgreSQL/RowsEnumerable.cs
+++ src/Pure.RelationalSchema.Storage.PostgreSQL/RowsEnumerable.cs
@@ -3,6 +3,7 @@ using System.Data;
 using Pure.Collections.Generic;
 using Pure.Primitives.Abstractions.String;
 using Pure.Primitives.Materialized.String;
+using Pure.Primitives.String.Operations;
 using Pure.RelationalSchema.Abstractions.Column;
 using Pure.RelationalSchema.Abstractions.Table;
 using Pure.RelationalSchema.HashCodes;
@@ -35,7 +36,9 @@ internal sealed record RowsEnumerable : IEnumerable<IRow>
         while (reader.Read())
         {
             IReadOnlyDictionary<string, string> rawCells = _table
-                .Columns.Select(x => new MaterializedString(x.Name).Value)
+                .Columns.Select(x =>
+                    new TrimmedHash(new HexString(new ColumnHash(x))).TextValue
+                )
                 .ToDictionary(x => x, x => reader[x].ToString())!;
 
             IReadOnlyDictionary<IColumn, ICell> cells = new Dictionary<
