d8842336e6fb4ab03d9c8968f9dcf5c0f0875c5b
Dmitry Kurochkin <kudima03@gmail.com>
2025-09-08 12:21:14 +0300

    Implemented InsertStatement

---
+++ src/Pure.RelationalSchema.Storage.PostgreSQL/InsertStatement.cs
@@ -0,0 +1,110 @@
+using System.Collections;
+using Pure.Primitives.Abstractions.Char;
+using Pure.Primitives.Abstractions.String;
+using Pure.Primitives.String;
+using Pure.Primitives.String.Operations;
+using Pure.RelationalSchema.Abstractions.Schema;
+using Pure.RelationalSchema.Abstractions.Table;
+using Pure.RelationalSchema.HashCodes;
+using Pure.RelationalSchema.Storage.Abstractions;
+using Char = Pure.Primitives.Char.Char;
+using String = Pure.Primitives.String.String;
+
+namespace Pure.RelationalSchema.Storage.PostgreSQL;
+
+internal sealed record InsertStatement : IString
+{
+    private readonly IString _tableName;
+
+    private readonly IString _schemaName;
+
+    private readonly IEnumerable<IRow> _rows;
+
+    public InsertStatement(ITable table, ISchema schema, IEnumerable<IRow> rows)
+        : this(
+            new HexString(new TableHash(table)),
+            new HexString(new SchemaHash(schema)),
+            rows
+        )
+    { }
+
+    public InsertStatement(IString tableName, IString schemaName, IEnumerable<IRow> rows)
+    {
+        _tableName = tableName;
+        _schemaName = schemaName;
+        _rows = rows;
+    }
+
+    public string TextValue =>
+        (
+            (IString)
+                new WhitespaceJoinedString(
+                    new String("INSERT INTO"),
+                    new JoinedString(
+                        new DotString(),
+                        [
+                            new WrappedString(
+                                new DoubleQuoteString(),
+                                new TrimmedHash(_schemaName)
+                            ),
+                            new WrappedString(
+                                new DoubleQuoteString(),
+                                new TrimmedHash(_tableName)
+                            ),
+                        ]
+                    ),
+                    new WrappedString(
+                        new LeftRoundBracketString(),
+                        new JoinedString(
+                            new ConcatenatedString(
+                                new CommaString(),
+                                new WhitespaceString()
+                            ),
+                            _rows
+                                .SelectMany(x => x.Cells.Keys)
+                                .Select(x => new HexString(new ColumnHash(x)))
+                                .DistinctBy(x => ((IString)x).TextValue)
+                                .Select(x => new WrappedString(
+                                    new DoubleQuoteString(),
+                                    new TrimmedHash(x)
+                                ))
+                        ),
+                        new RightRoundBracketString()
+                    ),
+                    new String("VALUES"),
+                    new ConcatenatedString(
+                        new JoinedString(
+                            new ConcatenatedString(
+                                new CommaString(),
+                                new WhitespaceString()
+                            ),
+                            _rows.Select(x => new WrappedString(
+                                new LeftRoundBracketString(),
+                                new JoinedString(
+                                    new ConcatenatedString(
+                                        new CommaString(),
+                                        new WhitespaceString()
+                                    ),
+                                    x.Cells.Values.Select(c => new WrappedString(
+                                        new SingleQuoteString(),
+                                        c.Value
+                                    ))
+                                ),
+                                new RightRoundBracketString()
+                            ))
+                        ),
+                        new String(";")
+                    )
+                )
+        ).TextValue;
+
+    public IEnumerator<IChar> GetEnumerator()
+    {
+        return TextValue.Select(x => new Char(x)).GetEnumerator();
+    }
+
+    IEnumerator IEnumerable.GetEnumerator()
+    {
+        return GetEnumerator();
+    }
+}
