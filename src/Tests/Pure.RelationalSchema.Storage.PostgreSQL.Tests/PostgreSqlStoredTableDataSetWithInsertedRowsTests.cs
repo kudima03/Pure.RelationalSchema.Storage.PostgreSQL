0da57406741c24e9d993aa4f2344531dcb65438e
Dmitry Kurochkin <kudima03@gmail.com>
2025-09-08 21:12:31 +0300

    PostgreSqlStoredTableDataSetWithInsertedRowsTests fixed.

--- src/Tests/Pure.RelationalSchema.Storage.PostgreSQL.Tests/PostgreSqlStoredTableDataSetWithInsertedRowsTests.cs
+++ src/Tests/Pure.RelationalSchema.Storage.PostgreSQL.Tests/PostgreSqlStoredTableDataSetWithInsertedRowsTests.cs
@@ -1,18 +1,8 @@
-using System.Collections;
 using Pure.Collections.Generic;
-using Pure.Primitives.Abstractions.Char;
-using Pure.Primitives.Abstractions.Number;
-using Pure.Primitives.Abstractions.String;
-using Pure.Primitives.Random.Date;
-using Pure.Primitives.Random.Number;
-using Pure.Primitives.Switches.String;
 using Pure.RelationalSchema.Abstractions.Column;
-using Pure.RelationalSchema.Abstractions.ColumnType;
-using Pure.RelationalSchema.ColumnType;
 using Pure.RelationalSchema.HashCodes;
 using Pure.RelationalSchema.Storage.Abstractions;
-using Char = Pure.Primitives.Char.Char;
-using String = Pure.Primitives.String.String;
+using Pure.RelationalSchema.Storage.PostgreSQL.Tests.Fakes;
 
 namespace Pure.RelationalSchema.Storage.PostgreSQL.Tests;
 
@@ -27,7 +17,7 @@ public sealed record PostgreSqlStoredTableDataSetWithInsertedRowsTests
     }
 
     [Fact]
-    public void EnumeratesAsTyped()
+    public void InsertMultipleRows()
     {
         IPostgreSqlStoredTableDataSet schema = new PostgreSqlStoredTableDataSet(
             _fixture.Schema.Tables.First(),
@@ -35,87 +25,20 @@ public sealed record PostgreSqlStoredTableDataSetWithInsertedRowsTests
             _fixture.Connection
         );
 
-        IEnumerable<IRow> rowsToInsert =
-        [
-            new Row(
+        IEnumerable<IRow> rowsToInsert = Enumerable
+            .Range(0, 100)
+            .Select(_ => new Row(
                 new Dictionary<IColumn, IColumn, ICell>(
                     schema.TableSchema.Columns,
                     x => x,
-                    x => new Cell(
-                        
-                    ),
+                    x => new Cell(new RandomValueForColumnType(x.Type)),
                     x => new ColumnHash(x)
                 )
-            ),
-        ];
+            ));
 
         IPostgreSqlStoredTableDataSet dataSetWithInsertedRows =
             new PostgreSqlStoredTableDataSetWithInsertedRows(schema, rowsToInsert);
 
-        Assert.Equal(1, dataSetWithInsertedRows.Count());
-    }
-}
-
-internal sealed record RandomValueForColumnType : IString
-{
-    private readonly IColumnType _columnType;
-
-    public RandomValueForColumnType(IColumnType columnType)
-    {
-        _columnType = columnType;
-    }
-
-    public string TextValue =>
-        new StringSwitch<IColumnType>(
-            _columnType,
-            [
-                new KeyValuePair<IColumnType, IString>(
-                    new IntColumnType(),
-                    ((INumber<int>)new RandomInt()).NumberValue.
-                ),
-                new KeyValuePair<IColumnType, IString>(
-                    new LongColumnType(),
-                    new String("bigint")
-                ),
-                new KeyValuePair<IColumnType, IString>(
-                    new UShortColumnType(),
-                    new String("integer")
-                ),
-                new KeyValuePair<IColumnType, IString>(
-                    new UIntColumnType(),
-                    new String("bigint")
-                ),
-                new KeyValuePair<IColumnType, IString>(
-                    new ULongColumnType(),
-                    new String("numeric")
-                ),
-                new KeyValuePair<IColumnType, IString>(
-                    new StringColumnType(),
-                    new String("text")
-                ),
-                new KeyValuePair<IColumnType, IString>(
-                    new DateColumnType(),
-                    new String("date")
-                ),
-                new KeyValuePair<IColumnType, IString>(
-                    new DateTimeColumnType(),
-                    new String("timestamp")
-                ),
-                new KeyValuePair<IColumnType, IString>(
-                    new TimeColumnType(),
-                    new String("time")
-                ),
-            ],
-            x => new ColumnTypeHash(x)
-        ).TextValue;
-
-    public IEnumerator<IChar> GetEnumerator()
-    {
-        return TextValue.Select(x => new Char(x)).GetEnumerator();
-    }
-
-    IEnumerator IEnumerable.GetEnumerator()
-    {
-        return GetEnumerator();
+        Assert.Equal(100, dataSetWithInsertedRows.Count());
     }
 }
