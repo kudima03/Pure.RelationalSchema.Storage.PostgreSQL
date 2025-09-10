81e8315db36467c58a533e793530c5dbb719e2a6
Dmitry Kurochkin <kudima03@gmail.com>
2025-09-10 14:35:43 +0300

    Added schema creation idempotence.

--- src/Pure.RelationalSchema.Storage.PostgreSQL/ForeignKeyCreationStatement.cs
+++ src/Pure.RelationalSchema.Storage.PostgreSQL/ForeignKeyCreationStatement.cs
@@ -1,10 +1,12 @@
 using System.Collections;
 using Pure.Primitives.Abstractions.Char;
 using Pure.Primitives.Abstractions.String;
+using Pure.Primitives.String;
 using Pure.Primitives.String.Operations;
 using Pure.RelationalSchema.Abstractions.ForeignKey;
 using Pure.RelationalSchema.HashCodes;
 using Char = Pure.Primitives.Char.Char;
+using String = Pure.Primitives.String.String;
 
 namespace Pure.RelationalSchema.Storage.PostgreSQL;
 
@@ -24,11 +26,61 @@ internal sealed record ForeignKeyCreationStatement : IString
         (
             (IString)
                 new NewLineJoinedString(
-                    new AlterTableStatement(_foreignKey.ReferencingTable, _schemaName),
-                    new AddConstraintStatement(
-                        new TrimmedHash(new HexString(new ForeignKeyHash(_foreignKey)))
+                    new String("DO $$"),
+                    new String("BEGIN"),
+                    new WhitespaceJoinedString(
+                        new String("IF NOT EXISTS"),
+                        new WrappedString(
+                            new LeftRoundBracketString(),
+                            new WhitespaceJoinedString(
+                                new String("SELECT"),
+                                new JoinedString(
+                                    new ConcatenatedString(
+                                        new CommaString(),
+                                        new WhitespaceString()
+                                    ),
+                                    [
+                                        new String("constraint_schema"),
+                                        new String("constraint_name"),
+                                    ]
+                                ),
+                                new String("FROM"),
+                                new String("information_schema.table_constraints"),
+                                new String("WHERE"),
+                                new String("constraint_schema"),
+                                new String("="),
+                                new WrappedString(
+                                    new SingleQuoteString(),
+                                    new TrimmedHash(_schemaName)
+                                ),
+                                new String("AND"),
+                                new String("constraint_name"),
+                                new String("="),
+                                new WrappedString(
+                                    new SingleQuoteString(),
+                                    new TrimmedHash(
+                                        new HexString(new ForeignKeyHash(_foreignKey))
+                                    )
+                                )
+                            ),
+                            new RightRoundBracketString()
+                        )
                     ),
-                    new ForeignKeyStatement(_foreignKey, _schemaName)
+                    new String("THEN"),
+                    new NewLineJoinedString(
+                        new AlterTableStatement(
+                            _foreignKey.ReferencingTable,
+                            _schemaName
+                        ),
+                        new AddConstraintStatement(
+                            new TrimmedHash(
+                                new HexString(new ForeignKeyHash(_foreignKey))
+                            )
+                        ),
+                        new ForeignKeyStatement(_foreignKey, _schemaName)
+                    ),
+                    new ConcatenatedString(new String("END IF"), new SemicolonString()),
+                    new ConcatenatedString(new String("END$$"), new SemicolonString())
                 )
         ).TextValue;
 
