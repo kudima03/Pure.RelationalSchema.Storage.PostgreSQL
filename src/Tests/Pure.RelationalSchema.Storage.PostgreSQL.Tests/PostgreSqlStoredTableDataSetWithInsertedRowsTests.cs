ad261a90df97aa3c1971cc01d3c6e7359fb51f8d
Dmitry Kurochkin <kudima03@gmail.com>
2025-09-29 13:08:56 +0300

    Added test for dictinct insertion.

--- src/Tests/Pure.RelationalSchema.Storage.PostgreSQL.Tests/PostgreSqlStoredTableDataSetWithInsertedRowsTests.cs
+++ src/Tests/Pure.RelationalSchema.Storage.PostgreSQL.Tests/PostgreSqlStoredTableDataSetWithInsertedRowsTests.cs
@@ -7,20 +7,16 @@ using Pure.RelationalSchema.Storage.PostgreSQL.Tests.Fakes;
 namespace Pure.RelationalSchema.Storage.PostgreSQL.Tests;
 
 public sealed record PostgreSqlStoredTableDataSetWithInsertedRowsTests
-    : IClassFixture<DatabaseFixture>
+    : IAsyncLifetime,
+        IDisposable
 {
-    private readonly DatabaseFixture _fixture;
-
-    public PostgreSqlStoredTableDataSetWithInsertedRowsTests(DatabaseFixture fixture)
-    {
-        _fixture = fixture;
-    }
+    private DatabaseFixture? _fixture;
 
     [Fact]
     public void InsertMultipleRows()
     {
         IPostgreSqlStoredTableDataSet schema = new PostgreSqlStoredTableDataSet(
-            _fixture.Schema.Tables.First(),
+            _fixture!.Schema.Tables.First(),
             _fixture.Schema,
             _fixture.Connection
         );
@@ -40,5 +36,48 @@ public sealed record PostgreSqlStoredTableDataSetWithInsertedRowsTests
             new PostgreSqlStoredTableDataSetWithInsertedRows(schema, rowsToInsert);
 
         Assert.Equal(100, dataSetWithInsertedRows.Count());
+    }
+
+    [Fact]
+    public void InsertOnlyDistinctRows()
+    {
+        IPostgreSqlStoredTableDataSet schema = new PostgreSqlStoredTableDataSet(
+            _fixture!.Schema.Tables.First(),
+            _fixture.Schema,
+            _fixture.Connection
+        );
+
+        IEnumerable<IRow> rowsToInsert = Enumerable
+            .Range(0, 100)
+            .Select(_ => new Row(
+                new Dictionary<IColumn, IColumn, ICell>(
+                    schema.TableSchema.Columns,
+                    x => x,
+                    x => new Cell(new DefaultValueForColumnType(x.Type)),
+                    x => new ColumnHash(x)
+                )
+            ));
+
+        IPostgreSqlStoredTableDataSet dataSetWithInsertedRows =
+            new PostgreSqlStoredTableDataSetWithInsertedRows(schema, rowsToInsert);
+
+        Assert.Equal(1, dataSetWithInsertedRows.Count());
+    }
+
+    public void Dispose()
+    {
+        _fixture?.Dispose();
+    }
+
+    public Task DisposeAsync()
+    {
+        Dispose();
+        return Task.CompletedTask;
+    }
+
+    public Task InitializeAsync()
+    {
+        _fixture = new DatabaseFixture();
+        return Task.CompletedTask;
     }
 }
